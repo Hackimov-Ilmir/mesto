011d50ca576611021eb566fe3302b291265ae6b8
Ilmir Khakimov <khakimoff.ilmir@yandex.ru>
2023-03-07 21:30:43 +0500

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,35 @@
+const editButton = document.querySelector(".profile__edit-button");
+const popupElement = document.querySelector(".popup");
+const popupClass = popupElement.classList;
+
+const closeButton = popupElement.querySelector(".popup__close-button");
+
+const nicknameValue = document.querySelector(".profile__info-nickname");
+const descriptionValue = document.querySelector(".profile__info-description");
+const saveButton = popupElement.querySelector(".form__save-button");
+const nicknameInput = popupElement.querySelector(".form__nickname");
+const descriptionInput = popupElement.querySelector(".form__description");
+const formElement = popupElement.querySelector(".form");
+
+function popupOpen() {
+  popupClass.add("popup_opened");
+  nicknameInput.value = nicknameValue.textContent;
+  descriptionInput.value = descriptionValue.textContent;
+}
+
+function popupClose() {
+  popupClass.remove("popup_opened");
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+
+  nicknameValue.textContent = nicknameInput.value;
+  descriptionValue.textContent = descriptionInput.value;
+
+  popupClose();
+}
+
+editButton.addEventListener("click", popupOpen);
+closeButton.addEventListener("click", popupClose);
+formElement.addEventListener("submit", handleFormSubmit);
