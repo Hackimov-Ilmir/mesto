b944228ecd0bdffd32e9b0a37d2440ea3a6bf66b
Ilmir Khakimov <khakimoff.ilmir@yandex.ru>
2023-03-08 19:04:59 +0500

    second review

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
-const editButton = document.querySelector(".profile__edit-button");
-const popupElement = document.querySelector(".popup");
+const editButton = document.querySelector('.profile__edit-button');
+const popupElement = document.querySelector('.popup');
 const popupClass = popupElement.classList;
 
-const closeButton = popupElement.querySelector(".popup__close-button");
+const closeButton = popupElement.querySelector('.popup__close-button');
 
-const nicknameValue = document.querySelector(".profile__info-nickname");
-const descriptionValue = document.querySelector(".profile__info-description");
-const saveButton = popupElement.querySelector(".form__save-button");
-const nicknameInput = popupElement.querySelector(".form__nickname");
-const descriptionInput = popupElement.querySelector(".form__description");
-const formElement = popupElement.querySelector(".form");
+const nicknameValue = document.querySelector('.profile__info-nickname');
+const descriptionValue = document.querySelector('.profile__info-description');
+const saveButton = popupElement.querySelector('.form__save-button');
+const nicknameInput = popupElement.querySelector('.form__input_type_nickname');
+const descriptionInput = popupElement.querySelector('.form__input_type_description');
+const formElement = popupElement.querySelector('.form');
 
 function popupOpen() {
-  popupClass.add("popup_opened");
+  popupClass.add('popup_opened');
   nicknameInput.value = nicknameValue.textContent;
   descriptionInput.value = descriptionValue.textContent;
 }
 
 function popupClose() {
-  popupClass.remove("popup_opened");
+  popupClass.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
@@ -30,6 +30,6 @@ function handleFormSubmit(evt) {
   popupClose();
 }
 
-editButton.addEventListener("click", popupOpen);
-closeButton.addEventListener("click", popupClose);
-formElement.addEventListener("submit", handleFormSubmit);
+editButton.addEventListener('click', popupOpen);
+closeButton.addEventListener('click', popupClose);
+formElement.addEventListener('submit', handleFormSubmit);
